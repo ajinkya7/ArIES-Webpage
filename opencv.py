0daa062f0fd5956f9ecf2d7825bd5e3e3937d643
Aayush61 <asinghal@mt.iitr.ac.in>
2020-02-07 22:10:20 +0530

    additions

--- opencv.py
+++ opencv.py
@@ -1,4 +1,4 @@
 import cv2
-img20 = cv2.imread("public/index/img/members/Aayush.jpg",cv2.IMREAD_COLOR)
+img20 = cv2.imread("public/index/img/memberfull/kanisk.jpg",cv2.IMREAD_COLOR)
 M=cv2.resize(img20,(362,362))
-cv2.imwrite("Aayush1.jpg",M)
+cv2.imwrite("kanisk.jpg",M)
